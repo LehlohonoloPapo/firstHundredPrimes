84291dc8fa4213493ade4609bcc3d4c3472137f7
LehlohonoloPapo <narepapo.12@gmail.com>
2022-08-03 19:03:22 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var count = 3;
+int number=1;
+//2 and 3 are defualt for a natural number n
+Console.Write(2 + "\t" + 3+ "\t");
+while (true)
+{
+ 
+    Console.Write( (6*number-1)+ "\t");
+    number++;
+    if (count == 100) break;
+    count++;
+}
